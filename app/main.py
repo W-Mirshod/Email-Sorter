c233ce7796de27201a242b2b1bbb4d34054c18c8
W Mirshod <python3526@gmail.com>
2025-11-05 03:01:43 +0500

    Enhance read_root function by adding type hint for Request parameter

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
@@ -13,5 +13,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request):
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
